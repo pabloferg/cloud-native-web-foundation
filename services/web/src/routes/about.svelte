a9b4ce565fb9cd83383e716815479e298449a72e
pabloferg <pablofergui1@gmail.com>
2020-10-22 16:11:49 +0000

    tailwindcss added

--- services/web/src/routes/about.svelte
+++ services/web/src/routes/about.svelte
@@ -1,6 +1,16 @@
 <script>
+  import ContactCard from "../components/contact-card.svelte";
   import FeatureToggle from "../components/feature-toggle.svelte";
   import { FEATURE_TOGGLES } from "../feature-toggles";
+
+  const erinLindford = {
+    name: "Erin Lindford",
+    avatarSrc: "https://randomuser.me/api/portraits/women/17.jpg",
+    job: "Customer Support",
+    email: "erinlindford@example.com",
+    phone: "(555) 765-4321"
+  }
+
 </script>
 
 
@@ -10,8 +20,10 @@
 
 <h1>About this site</h1>
 
-<p>This is the 'about' page. There's not much here.</p>
+<p class='text-red-600'>Thanks Mike for your help!</p>
 
 <FeatureToggle name={FEATURE_TOGGLES.vipWelcome.name}>
   <p>Some people see this, others don't!</p>
 </FeatureToggle>
+
+<ContactCard {...erinLindford} />
