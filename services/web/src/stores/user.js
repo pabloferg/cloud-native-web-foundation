0ebe33694754fac6296e4872d2e8d995dcd1a48d
pabloferg <pablofergui1@gmail.com>
2020-10-22 21:12:12 +0000

    authentication chapter finished

---
+++ services/web/src/stores/user.js
@@ -0,0 +1,13 @@
+import { writable } from "svelte/store";
+
+const createStore = () => {
+  const { subscribe, set } = writable(null);
+
+  return {
+    subscribe,
+    signIn: (user) => set(user),
+    signOut: () => set(null),
+  };
+};
+
+export const currentUser = createStore();
