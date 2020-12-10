c82e0d32caddb109566a9bb10f562f226f786994
pabloferg <pablofergui1@gmail.com>
2020-12-10 18:51:55 +0000

    new page added, with new tailwindcss designs

--- services/web/src/components/tailwindcss.svelte
+++ services/web/src/components/tailwindcss.svelte
@@ -1,5 +1,11 @@
 <style global>
   @tailwind base;
+  html {
+      @apply font-sans;
+      @apply antialiased;
+      @apply bg-gray-100;
+      @apply text-gray-900;
+  }
   @tailwind components;
   @tailwind utilities;
 </style> 
